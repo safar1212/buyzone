1b49d26b52ffe93a32f9910cae27e2f369c98dfd
Safar Ali <asafar880@gmail.com>
2023-11-02 23:29:26 +0500

    complete header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Header from './header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,11 @@
+
+export default function Header(){
+    return(
+        <div>
+            <header>
+                <h1>Hi there, it is h1 for header</h1>
+            </header>
+        </div>
+
+    )
+}
