647c2ceb1b6d29f801dade80808cdb55f278c9ef
Safar Ali <asafar880@gmail.com>
2023-11-04 23:21:20 +0500

    Learn how to use useState to change the UI when interacting

--- src/App.js
+++ src/App.js
@@ -3,17 +3,40 @@ import './App.css';
 import Header from './Header';
 import data from './memesData';
 import Meme from './Memes';
+import react, {useState} from "react";
 
 
 
 
 function App() {
 
+  
+
+  const [thingsArray, setThingsArray] = useState(["thing 1", "thing 2"])
+
+  console.log(useState);
+
+
+  function addItem(){
+    setThingsArray(PrevthingsArray => {
+      return [...PrevthingsArray, `thing ${PrevthingsArray.length + 1}`]
+    })
+  }
+
+
+   let thingsElement = thingsArray.map(thing =><p>{thing}</p>)
+
+
+
   return (
     <div className="App">
       <Header />
       <Meme />
+      <div onClick={addItem}>
+        {thingsElement}
+      </div>
     </div>
+    
   );
 
 }
