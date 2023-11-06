88b92e33d828b23003d706c188d9dd0dc83e59e5
Safar Ali <asafar880@gmail.com>
2023-11-06 13:32:53 +0500

    Add functions to rendomly choose memes

--- src/App.js
+++ src/App.js
@@ -10,38 +10,12 @@ import react, {useState} from "react";
 
 function App() {
 
- 
-
-  const [thingsArray, setThingsArray] = useState([])
-
-
-
-  function addItem(){
-    
-    setThingsArray(prevThingsArray => {
-      return [ ...prevThingsArray, `Thing ${prevThingsArray.length + 1}`];
-      
-    })
-
-    // setThingsArray(prevThingsArray =>{
-    //   return [prevThingsArray.push("Thing 3")];
-    // })
-  }
-
-
-   let thingsElement = thingsArray.map(thing =><p key={thing}>{thing}</p>)
-
-
 
   return (
     <div className="App">
       <Header />
-      <Meme />
-      <div>
-        <button type='submit' onClick={addItem}>Click me</button>
-      </div>
       <div >
-        {thingsElement}
+       <Meme />
       </div>
     </div>
     
