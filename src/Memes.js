ae87f53884043a8e68dcef4334417d4bf18d6bc7
Safar Ali <asafar880@gmail.com>
2023-11-11 18:29:59 +0500

    Edit the html form

--- src/Memes.js
+++ src/Memes.js
@@ -27,7 +27,7 @@ export default function Meme() {
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
         <button className="form--button" onClick={getMemeImage}>
-          Get a new meme image 🖼
+          Get a new image 🖼
         </button>
       </div>
       <img src={meme.randomImage} className="meme--image" />
